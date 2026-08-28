0498256a817ac174b0c398995890f0c8bf88e5f4
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-08-28 17:20:39 +0200

    fix: use Compose native key event member

--- app/src/main/java/com/gamebox/os/ui/GameBoxApp.kt
+++ app/src/main/java/com/gamebox/os/ui/GameBoxApp.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.nativeKeyEvent
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.text.font.FontWeight
