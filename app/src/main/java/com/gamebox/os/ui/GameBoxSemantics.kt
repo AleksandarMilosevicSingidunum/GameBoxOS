bdfe5d0e25913e5fe2c5374c2791b65e9cc6d2dc
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-08-29 14:50:04 +0200

    test: define stable ui semantics identifiers

---
+++ app/src/main/java/com/gamebox/os/ui/GameBoxSemantics.kt
@@ -0,0 +1,11 @@
+package com.gamebox.os.ui
+
+object GameBoxSemantics {
+    const val HOME_TAB = "gamebox.home.tab"
+    const val LIBRARY_TAB = "gamebox.library.tab"
+    const val STORE_TAB = "gamebox.store.tab"
+    const val SETTINGS_TAB = "gamebox.settings.tab"
+    const val GAME_CARD = "gamebox.game.card"
+    const val PRIMARY_ACTION = "gamebox.primary.action"
+    const val BACK_ACTION = "gamebox.back.action"
+}
