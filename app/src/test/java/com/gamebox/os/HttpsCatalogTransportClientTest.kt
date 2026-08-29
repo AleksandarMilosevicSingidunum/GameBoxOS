2b69a1fe37fac6a8dbad0bc730faff213fbee3d2
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-08-29 14:00:48 +0200

    test: cover unsigned s3 credential rejection

---
+++ app/src/test/java/com/gamebox/os/HttpsCatalogTransportClientTest.kt
@@ -0,0 +1,17 @@
+package com.gamebox.os
+
+import com.gamebox.os.catalog.CatalogCredentials
+import com.gamebox.os.catalog.CatalogTransport
+import com.gamebox.os.catalog.HttpsCatalogTransportClient
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class HttpsCatalogTransportClientTest {
+    @Test
+    fun unsignedS3CredentialsFailBeforeNetworkAccess() = runBlocking {
+        assertThrows(IllegalArgumentException::class.java) {
+            runBlocking { HttpsCatalogTransportClient().fetch(CatalogTransport.S3("https://example.test", "games"), CatalogCredentials(accessKey = "key", secretKey = "secret")) }
+        }
+    }
+}
