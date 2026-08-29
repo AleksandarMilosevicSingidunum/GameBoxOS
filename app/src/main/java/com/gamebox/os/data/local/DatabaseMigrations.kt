9f578c1d8776f266de61606d4bd6de09a0c755d8
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-08-29 12:10:37 +0200

    feat: add favorites database migration

--- app/src/main/java/com/gamebox/os/data/local/DatabaseMigrations.kt
+++ app/src/main/java/com/gamebox/os/data/local/DatabaseMigrations.kt
@@ -35,3 +35,12 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
         )
     }
 }
+
+
+val MIGRATION_3_4 = object : Migration(3, 4) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE games ADD COLUMN favorite INTEGER NOT NULL DEFAULT 0"
+        )
+    }
+}
