44260894073fff47169f534f9782b8afb4a0da51
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-08-30 22:49:55 +0200

    Cover offline operation policy branches

--- app/src/test/java/com/gamebox/os/OfflineOperationPolicyTest.kt
+++ app/src/test/java/com/gamebox/os/OfflineOperationPolicyTest.kt
@@ -7,6 +7,24 @@ import org.junit.Test
 
 class OfflineOperationPolicyTest {
     @Test
+    fun uncachedNetworkDataRequiresNetworkOffline() {
+        val policy = OfflineOperationPolicy { true }
+        assertEquals(OfflineAction.REQUIRE_NETWORK, policy.actionFor(true, false))
+    }
+
+    @Test
+    fun localOnlyOperationRemainsAvailableOffline() {
+        val policy = OfflineOperationPolicy { true }
+        assertEquals(OfflineAction.OPEN_LOCAL_LIBRARY, policy.actionFor(false, false))
+    }
+
+    @Test
+    fun onlineNetworkOperationRequiresNetworkInsteadOfCache() {
+        val policy = OfflineOperationPolicy { false }
+        assertEquals(OfflineAction.REQUIRE_NETWORK, policy.actionFor(true, true))
+    }
+
+    @Test
     fun cachedNetworkDataIsUsedOffline() {
         val policy = OfflineOperationPolicy { true }
         assertEquals(OfflineAction.USE_CACHED_CATALOG, policy.actionFor(true, true))
