f90e1d7d35eafbaaca447742c6c4e7099dabd3f5
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-09-11 19:57:07 +0200

    Fix controller overlay test imports

--- app/src/androidTest/java/com/gamebox/os/ui/ControllerOverlayTest.kt
+++ app/src/androidTest/java/com/gamebox/os/ui/ControllerOverlayTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.gamebox.os.MainActivity
 import org.junit.Rule
