55fdc2c0ba34a3413a2ea6937f37f0079d0cca13
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-09-11 19:47:23 +0200

    Wire analog stick navigation into MainActivity

--- app/src/main/java/com/gamebox/os/MainActivity.kt
+++ app/src/main/java/com/gamebox/os/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.gamebox.os
 
 import android.os.Bundle
+import android.view.InputDevice
+import android.view.KeyEvent
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -9,12 +12,14 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.background
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
+import com.gamebox.os.input.AnalogNavigationDebouncer
 import com.gamebox.os.ui.GameBoxApp
 import com.gamebox.os.ui.OfflineStatusBanner
 import com.gamebox.os.ui.theme.GameBoxTheme
 
 class MainActivity : ComponentActivity() {
     private val container by lazy { (application as GameBoxApplication).container }
+    private val analogNavigation = AnalogNavigationDebouncer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +46,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun dispatchGenericMotionEvent(event: MotionEvent): Boolean {
+        val joystick = event.source and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK
+        if (joystick && event.action == MotionEvent.ACTION_MOVE) {
+            val keyCode = analogNavigation.consume(
+                stickX = event.getAxisValue(MotionEvent.AXIS_X),
+                stickY = event.getAxisValue(MotionEvent.AXIS_Y),
+                hatX = event.getAxisValue(MotionEvent.AXIS_HAT_X),
+                hatY = event.getAxisValue(MotionEvent.AXIS_HAT_Y),
+                eventTimeMs = event.eventTime,
+            )
+            if (keyCode != null) {
+                val down = KeyEvent(event.downTime, event.eventTime, KeyEvent.ACTION_DOWN, keyCode, 0)
+                val up = KeyEvent(event.downTime, event.eventTime, KeyEvent.ACTION_UP, keyCode, 0)
+                super.dispatchKeyEvent(down)
+                super.dispatchKeyEvent(up)
+                return true
+            }
+        }
+        return super.dispatchGenericMotionEvent(event)
+    }
+
     override fun onPause() {
+        analogNavigation.reset()
         container.gameLaunchController.onHostPaused()
         super.onPause()
     }
