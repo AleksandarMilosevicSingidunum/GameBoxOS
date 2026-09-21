e1c89853223669a331b26261733216be75acb45c
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-09-21 14:23:19 +0200

    Register metadata provenance migration

--- app/src/main/java/com/gamebox/os/data/AppContainer.kt
+++ app/src/main/java/com/gamebox/os/data/AppContainer.kt
@@ -26,6 +26,7 @@ import com.gamebox.os.data.local.MIGRATION_9_10
 import com.gamebox.os.data.local.MIGRATION_10_11
 import com.gamebox.os.data.local.MIGRATION_11_12
 import com.gamebox.os.data.local.MIGRATION_12_13
+import com.gamebox.os.data.local.MIGRATION_13_14
 import com.gamebox.os.data.local.RoomLaunchSessionJournal
 import com.gamebox.os.download.AuthorizedDownloadController
 import com.gamebox.os.download.WorkManagerAuthorizedDownloadController
@@ -87,7 +88,7 @@ class DefaultAppContainer(context: Context) : AppContainer {
     ).addMigrations(
         MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5,
         MIGRATION_5_6, MIGRATION_6_7, MIGRATION_7_8, MIGRATION_8_9, MIGRATION_9_10,
-        MIGRATION_10_11, MIGRATION_11_12, MIGRATION_12_13,
+        MIGRATION_10_11, MIGRATION_11_12, MIGRATION_12_13, MIGRATION_13_14,
     ).build()
     override val settingsRepository = SettingsRepository(applicationContext)
     private val settingsState = settingsRepository.settings.stateIn(
