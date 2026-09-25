43e941de3236c5d113df3567cef0b418d5170443
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-09-25 11:59:51 +0200

    Test default Home role helpers

---
+++ app/src/test/java/com/gamebox/os/ui/HomeRoleStatusTest.kt
@@ -0,0 +1,22 @@
+package com.gamebox.os.ui
+
+import android.provider.Settings
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class HomeRoleStatusTest {
+    @Test
+    fun matchesOnlyTheGameBoxPackage() {
+        assertTrue(defaultHomeMatchesApp("com.gamebox.os", "com.gamebox.os"))
+        assertFalse(defaultHomeMatchesApp("com.android.launcher", "com.gamebox.os"))
+        assertFalse(defaultHomeMatchesApp(null, "com.gamebox.os"))
+        assertFalse(defaultHomeMatchesApp("", "com.gamebox.os"))
+    }
+
+    @Test
+    fun settingsIntentTargetsAndroidHomeSelection() {
+        assertEquals(Settings.ACTION_HOME_SETTINGS, defaultHomeSettingsIntent().action)
+    }
+}
