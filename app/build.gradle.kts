2b663dc00d2b352e6eb64b6f29d31740828b9d6d
Александар Милошевић <94201869+AleksandarMilosevicSingidunum@users.noreply.github.com>
2026-09-25 14:09:51 +0200

    Add UiAutomator for Android document picker tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
 
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
